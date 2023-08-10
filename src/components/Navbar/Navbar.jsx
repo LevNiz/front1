73c0668fbd231fdbb79f9bd0138c5f718333c6b4
beks <turgunbaaev@gmail.com>
2023-08-10 10:00:35 +0300

    Fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,8 +6,13 @@ import notification from './../../assets/icons/notification.svg';
 
 const Navbar = () => {
   const [loginModal, setLoginModal] = useState(false);
+  const [size, setSize] = useState(window.innerWidth);
   const modalRef = useRef();
-  const authorized = false;
+  const authorized = true;
+
+  window.addEventListener('resize', function () {
+    setSize(window.innerWidth);
+  });
 
   const handleOutSideModal = (e) => {
     if (!modalRef.current.contains(e.target)) {
@@ -27,71 +32,79 @@ const Navbar = () => {
   }, [loginModal]);
 
   return (
-    <header className='bg-black py-2 relative'>
-      <div className='container flex justify-between items-center'>
-        <NavLink to='/'>
-          <img src={logo} alt='*' />
-        </NavLink>
-        <ul className='flex space-x-5 items-center text-white navbar'>
-          <li>
-            <NavLink to='/'>Главная</NavLink>
-          </li>
-          <li>
-            <NavLink to='tracking'>Трекинг посылок</NavLink>
-          </li>
-          <li>
-            <NavLink to='warehouses'>Наши склады</NavLink>
-          </li>
-        </ul>
-        {authorized ? (
-          <ul className='flex items-center justify-end space-x-4'>
-            <li>
-              <NavLink to='profile/personal-data'>
-                <img src={user} alt='*' />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to='profile/notifications'>
-                <img src={notification} alt='*' />
-              </NavLink>
-            </li>
-          </ul>
-        ) : (
-          <button
-            onClick={() => setLoginModal(true)}
-            className='bg-colYellow w-24 h-10 rounded-lg hover:bg-colYellowHover duration-100'
-          >
-            Войти
-          </button>
-        )}
-      </div>
-      {loginModal ? (
-        <div
-          onClick={(e) => handleOutSideModal(e)}
-          className='fixed top-0 left-0 w-full h-full bg-black bg-opacity-30 z-[99]'
-        >
-          <div
-            ref={modalRef}
-            className='absolute top-[100px] right-10 max-w-[370px] w-full bg-white z-[999] p-8 flex flex-col space-y-5 rounded-2xl'
-          >
-            <NavLink
-              to='login'
-              className='w-full p-3 bg-black text-white rounded-lg text-center hover:opacity-80 duration-100'
-            >
-              Войти
-            </NavLink>
-            <NavLink
-              to='register'
-              className='w-full p-3 bg-black text-white rounded-lg text-center hover:opacity-80 duration-100'
-            >
-              Зарегистрироваться
-            </NavLink>
-          </div>
+    <>
+      <header className='bg-black py-2 relative'>
+        <div className='container flex justify-between items-center'>
+          <NavLink to='/'>
+            <img src={logo} alt='*' />
+          </NavLink>
+          {size >= 768 ? (
+            <>
+              <ul className='flex space-x-5 items-center text-white navbar'>
+                <li>
+                  <NavLink to='/'>Главная</NavLink>
+                </li>
+                <li>
+                  <NavLink to='tracking'>Трекинг посылок</NavLink>
+                </li>
+                <li>
+                  <NavLink to='warehouses'>Наши склады</NavLink>
+                </li>
+              </ul>
+              {authorized ? (
+                <ul className='flex items-center justify-end space-x-4'>
+                  <li>
+                    <NavLink to='profile/personal-data'>
+                      <img src={user} alt='*' />
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to='profile/notifications'>
+                      <img src={notification} alt='*' />
+                    </NavLink>
+                  </li>
+                </ul>
+              ) : (
+                <button
+                  onClick={() => setLoginModal(true)}
+                  className='bg-colYellow w-24 h-10 rounded-lg hover:bg-colYellowHover duration-100'
+                >
+                  Войти
+                </button>
+              )}
+            </>
+          ) : (
+            ''
+          )}
         </div>
-      ) : (
-        ''
-      )}
-    </header>
+        {loginModal ? (
+          <div
+            onClick={(e) => handleOutSideModal(e)}
+            className='fixed top-0 left-0 w-full h-full bg-black bg-opacity-30 z-[99]'
+          >
+            <div
+              ref={modalRef}
+              className='absolute top-[100px] right-10 max-w-[370px] w-full bg-white z-[999] p-8 flex flex-col space-y-5 rounded-2xl'
+            >
+              <NavLink
+                to='login'
+                className='w-full p-3 bg-black text-white rounded-lg text-center hover:opacity-80 duration-100'
+              >
+                Войти
+              </NavLink>
+              <NavLink
+                to='register'
+                className='w-full p-3 bg-black text-white rounded-lg text-center hover:opacity-80 duration-100'
+              >
+                Зарегистрироваться
+              </NavLink>
+            </div>
+          </div>
+        ) : (
+          ''
+        )}
+      </header>
+    </>
   );
 };
 
