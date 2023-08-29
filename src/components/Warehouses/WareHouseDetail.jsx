4971cb4837e79c8828da2471daafbba9811a54ec
beks <turgunbaaev@gmail.com>
2023-08-29 13:20:09 +0300

    Fix warehouses image

--- src/components/Warehouses/WareHouseDetail.jsx
+++ src/components/Warehouses/WareHouseDetail.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Mousewheel } from 'swiper/modules';
 import 'swiper/css';
-import { warehouses } from '../../constants/wareHouseData';
 import location from './../../assets/icons/location.svg';
 import clock from './../../assets/icons/clock.svg';
 import call from './../../assets/icons/call.svg';
@@ -18,8 +17,10 @@ const WareHouseDetail = () => {
   const [loading, setLoading] = useState(false);
   const { id } = useParams();
 
-  const images = warehouses[0]?.images;
-  const [mainImg, setMainImg] = useState(images[0].image);
+  const images = warehouseItem?.images;
+  const [mainImg, setMainImg] = useState(
+    images == null ? '' : images[0]?.image
+  );
 
   const handleClick = (index) => {
     const main = images[index]?.image;
@@ -52,7 +53,7 @@ const WareHouseDetail = () => {
             <div className='w-full md:w-3/6 xl:w-2/5 mb-12 md:mb-0'>
               <div className='md:max-w-[472px] h-[320px] sm:h-[400px] overflow-hidden rounded-lg mx-auto'>
                 <img
-                  src={mainImg}
+                  src={warehouseItem?.images == null ? noImg : mainImg}
                   alt='*'
                   className='w-full h-full object-cover'
                 />
@@ -66,21 +67,23 @@ const WareHouseDetail = () => {
                 wrapperClass='swiper-wrapper justify-between sm:justify-center sm:space-x-4 lg:space-x-8'
                 className='flex justify-around mt-5'
               >
-                {images?.map((el, index) => (
-                  <SwiperSlide
-                    key={el.id}
-                    className='sm:max-w-[80px] !w-[22%] h-[50px] xs:h-[70px] sm:w-full sm:h-[80px] rounded-lg overflow-hidden cursor-pointer'
-                    onClick={() => {
-                      handleClick(index);
-                    }}
-                  >
-                    <img
-                      src={el.image}
-                      alt='*'
-                      className='w-full h-full object-cover'
-                    />
-                  </SwiperSlide>
-                ))}
+                {warehouseItem?.images !== null
+                  ? images?.map((el, index) => (
+                      <SwiperSlide
+                        key={el.id}
+                        className='sm:max-w-[80px] !w-[22%] h-[50px] xs:h-[70px] sm:w-full sm:h-[80px] rounded-lg overflow-hidden cursor-pointer'
+                        onClick={() => {
+                          handleClick(index);
+                        }}
+                      >
+                        <img
+                          src={el?.image}
+                          alt='*'
+                          className='w-full h-full object-cover'
+                        />
+                      </SwiperSlide>
+                    ))
+                  : ''}
               </Swiper>
             </div>
             <div className='md:w-3/6 xl:w-3/5'>
@@ -113,7 +116,7 @@ const WareHouseDetail = () => {
                   <span className='min-w-[40px] md:min-w-[50px] h-[40px] md:h-[50px] rounded-xl flex items-center justify-center bg-white'>
                     <img src={clock} alt='*' />
                   </span>
-                  <p className='text-base sm:text-xl xl:text-2xl font-medium ml-5'>
+                  <div className='text-base sm:text-xl xl:text-2xl font-medium ml-5'>
                     {warehouseItem?.workingHours?.map((el, index) => (
                       <div className='grid grid-cols-2' key={index}>
                         <div className='flex'>
@@ -154,7 +157,7 @@ const WareHouseDetail = () => {
                         </div>
                       </div>
                     ))}
-                  </p>
+                  </div>
                 </div>
               </div>
             </div>
