16303b7129558304f228ea72f377a711911d65ac
beks <turgunbaaev@gmail.com>
2023-10-29 00:04:10 +0300

    Remove local date-fns

--- src/helpers/FormatDate/formatDate.js
+++ src/helpers/FormatDate/formatDate.js
@@ -1,15 +1,14 @@
 import { format, isToday } from 'date-fns';
-import { ru } from 'date-fns/locale';
 
 const FormatDate = (dateFormat) => {
   const date = new Date(
     dateFormat?.seconds * 1000 + dateFormat?.nanoseconds / 1000000
   );
-  const formattedTime = format(date, 'H:mm', { locale: ru });
+  const formattedTime = format(date, 'H:mm');
   if (isToday(date)) {
     return formattedTime;
   } else {
-    return format(date, 'd MMMM yyyy', { locale: ru });
+    return format(date, 'd MMMM yyyy');
   }
 };
 
