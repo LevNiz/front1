157ce72a00551be5eeda105902b8af9a94e205eb
beks <turgunbaaev@gmail.com>
2023-12-05 10:08:57 +0300

    Search request changes

--- src/components/Profile/ItemSearchRequest/ItemSearchRequestNew.jsx
+++ src/components/Profile/ItemSearchRequest/ItemSearchRequestNew.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { fetchUser } from '../../../api/client';
 import { postSearchRequest } from '../../../api/searchRequest';
 import { ContentLoading } from '../../../helpers/Loader/Loader';
-import imgFile from '../../../assets/icons/photo.svg';
+import noImg from '../../../assets/images/no-image.svg';
 
 const ItemSearchRequestNew = () => {
   const { userID } = useSelector((state) => state?.user);
@@ -34,6 +34,7 @@ const ItemSearchRequestNew = () => {
   });
 
   const onSubmit = async (data) => {
+    console.log(data);
     setIsLoading(true);
     const { success } = await postSearchRequest(data, userID, fileValue);
     if (success) {
@@ -55,83 +56,91 @@ const ItemSearchRequestNew = () => {
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className='grid ld:grid-cols-2 ld:gap-5'>
             <div>
-              <div>
-                <p className='font-medium mb-2'>ФИО</p>
-                <input
-                  className='w-full border border-colGray2 p-[14px] rounded-[4px] focus:border-black focus:outline-none'
-                  placeholder='ФИО'
-                  {...register('name', {
-                    required: 'Поле обязательно к заполнению!',
-                  })}
-                />
-                {errors?.name && (
-                  <p className='text-red-500 mt-1 text-sm'>
-                    {errors?.name?.message || 'Поле обязательно к заполнению!'}
-                  </p>
-                )}
-              </div>
-              <div className='mt-4'>
-                <p className='font-medium mb-2'>Номер телефона</p>
-                <input
-                  className='w-full border border-colGray2 p-[16px] mm:p-[14px] rounded-[4px] focus:border-black focus:outline-none'
-                  placeholder='Номер телефона'
-                  type='tel'
-                  {...register('phone', {
-                    required: 'Поле обязательно к заполнению!',
-                    pattern: {
-                      value: /^[\d()+ -]+$/,
-                      message: 'Введите только цифры!',
-                    },
-                  })}
-                />
-                {errors?.phone && (
-                  <p className='text-red-500 mt-1 text-sm'>
-                    {errors?.phone?.message || 'Поле обязательно к заполнению!'}
-                  </p>
-                )}
-              </div>
-              <div className='mt-4'>
-                <p className='font-medium mb-2'>Доп. информация</p>
-                <textarea
-                  className='w-full border border-colGray2 p-[14px] rounded-[4px] focus:border-black focus:outline-none resize-none'
-                  placeholder='Дополнительная информация'
-                  {...register('description')}
-                />
-              </div>
+              <p className='font-medium mb-2'>ФИО</p>
+              <input
+                className='w-full border border-colGray2 p-[14px] rounded-[4px] focus:border-black focus:outline-none'
+                placeholder='ФИО'
+                {...register('name', {
+                  required: 'Поле обязательно к заполнению!',
+                })}
+              />
+              {errors?.name && (
+                <p className='text-red-500 mt-1 text-sm'>
+                  {errors?.name?.message || 'Поле обязательно к заполнению!'}
+                </p>
+              )}
             </div>
-            <div className='mt-2 ld:mt-0'>
-              <p className='font-medium mb-2'>Фото товара</p>
-              <label htmlFor='fileVal'>
-                <input
-                  className='hidden'
-                  id='fileVal'
-                  type='file'
-                  onChange={(e) => setFileValue(e.target.files[0])}
-                  accept='image/*'
-                />
-                <div className='border-dashed border-2 ld:min-h-[155px] border-colGray2 p-5 flex justify-center items-center cursor-pointer rounded-md'>
-                  <div className='py-2'>
-                    <img className='mx-auto' src={imgFile} alt='*' />
-                    <p className='opacity-50 pt-2'>Загрузить фото</p>
-                  </div>
-                </div>
-              </label>
-              {fileValue ? (
-                <div className='mt-5 h-48 overflow-hidden'>
-                  <img
-                    className='w-full h-full object-contain'
-                    src={URL.createObjectURL(fileValue)}
-                    alt='*'
-                  />
-                </div>
-              ) : (
-                ''
+            <div className='mt-3 ld:mt-0'>
+              <p className='font-medium mb-2'>Номер телефона</p>
+              <input
+                className='w-full border border-colGray2 p-[16px] mm:p-[14px] rounded-[4px] focus:border-black focus:outline-none'
+                placeholder='Номер телефона'
+                type='tel'
+                {...register('phone', {
+                  required: 'Поле обязательно к заполнению!',
+                  pattern: {
+                    value: /^[\d()+ -]+$/,
+                    message: 'Введите только цифры!',
+                  },
+                })}
+              />
+              {errors?.phone && (
+                <p className='text-red-500 mt-1 text-sm'>
+                  {errors?.phone?.message || 'Поле обязательно к заполнению!'}
+                </p>
               )}
             </div>
           </div>
+          <h3 className='pt-4 pb-2 font-medium text-lg'>Товары</h3>
+          <div className='grid ld:grid-cols-2 ld:gap-5 border border-gray-400 p-3 rounded-md'>
+            <div className='mt-3 ld:mt-0'>
+              <p className='font-medium mb-2'>Фото товара</p>
+              <div className='flex items-center'>
+                <label className='w-4/5' htmlFor='photo'>
+                  <input
+                    className='hidden'
+                    id='photo'
+                    type='file'
+                    onChange={(e) => setFileValue(e.target.files[0])}
+                    accept='image/*'
+                  />
+                  <div className='border-dashed border-2 h-[78px] border-colGray2 flex justify-center items-center cursor-pointer rounded-md'>
+                    <p className='opacity-50'>Загрузить фото</p>
+                  </div>
+                </label>
+                <div className='ml-3 overflow-hidden w-1/5 h-[78px] bg-gray-100 rounded-md'>
+                  <img
+                    className='w-full h-full object-contain'
+                    onError={(e) => {
+                      e.target.onerror = null;
+                      e.target.src = noImg;
+                    }}
+                    src={fileValue ? URL.createObjectURL(fileValue) : ''}
+                    alt='*'
+                  />
+                </div>
+              </div>
+            </div>
+            <div className='mt-3 ld:mt-0'>
+              <p className='font-medium mb-2'>Доп. информация</p>
+              <textarea
+                className='w-full border border-colGray2 p-[14px] rounded-[4px] focus:border-black focus:outline-none resize-none'
+                placeholder='Дополнительная информация'
+                {...register('description', {
+                  required: true,
+                })}
+              />
+            </div>
+          </div>
+          <div
+            // onClick={() => append({})}
+            className='bg-green-500 text-white font-medium rounded-md px-5 py-[2px] mt-3 flex ml-auto text-xl hover:opacity-80 duration-150 w-max cursor-pointer'
+          >
+            +
+          </div>
           <button
             type='submit'
-            className='mt-8 font-medium hover:opacity-80 p-3 rounded-lg bg-black text-white duration-150 max-w-sm ml-auto w-full flex justify-center'
+            className='mt-8 font-medium hover:opacity-80 p-3 rounded-lg bg-black text-white duration-150 max-w-xs ml-auto w-full flex justify-center'
           >
             Cохранить
           </button>
