78eee5cef7ed7f8bed76b6540373f952e82852d9
beks <turgunbaaev@gmail.com>
2023-12-21 20:04:23 +0300

    Add userID for PrivateRoute

--- src/helpers/Private/PrivateRoute.jsx
+++ src/helpers/Private/PrivateRoute.jsx
@@ -1,10 +1,12 @@
+import { useSelector } from 'react-redux';
 import { Navigate, Outlet, useOutletContext } from 'react-router-dom';
 
 const PrivateRoute = () => {
   const hasNotification = useOutletContext();
   const token = localStorage.getItem('accessToken');
+  const { userID } = useSelector((state) => state?.user);
 
-  return token ? (
+  return token && userID ? (
     <Outlet context={{ hasNotification }} />
   ) : (
     <Navigate to='/auth/sign-in' />
