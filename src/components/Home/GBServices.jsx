56734f64804746e1d15cf86d43e304f631e791ba
beks <turgunbaaev@gmail.com>
2024-01-25 15:33:31 +0300

    GB Services comment GB Pay

--- src/components/Home/GBServices.jsx
+++ src/components/Home/GBServices.jsx
@@ -1,6 +1,6 @@
 import bgImg from '../../assets/images/services-bg.jpg';
 import gbShop from '../../assets/icons/gb-services/gb-shop.svg';
-import gbPay from '../../assets/icons/gb-services/gb-pay.svg';
+// import gbPay from '../../assets/icons/gb-services/gb-pay.svg';
 import gbBusiness from '../../assets/icons/gb-services/gb-business.svg';
 import gbFranchise from '../../assets/icons/gb-services/gb-franchise.svg';
 import gbBuyer from '../../assets/icons/gb-services/gb-buyer.svg';
@@ -88,6 +88,29 @@ const GBServices = () => {
           </div>
         </NavLink>
         <NavLink
+          to='/gb-chat'
+          className='relative group min-h-[180px] ss:min-h-[220px] py-2 flex justify-center items-center bg-gbChat'
+        >
+          <div>
+            <img className='mx-auto w-10 sm:w-auto' src={gbChat} alt='*' />
+            <h3 className='font-medium sm:font-bold text-lg mm:text-2xl lg:text-3xl text-center mt-2'>
+              GB-Chat
+            </h3>
+            <p className='lg:hidden text-center p-2 text-[#FBFBFB] opacity-80 text-[10px] sm:text-sm'>
+              Не оставит вас одного с кучей важных вопросов. Простой и
+              практичный чат созданный для всех наших клиентов и в частности
+              именно Вас
+            </p>
+          </div>
+          <div className='hidden lg:flex group-hover:opacity-[1] opacity-0 duration-200 absolute top-0 left-0 w-full h-full justify-center items-center bg-[linear-gradient(180deg,_rgba(2,_1,_5,_0.58)_0%,_#020105_100%)]'>
+            <p className='text-center p-3 text-[#FBFBFB]'>
+              Не оставит вас одного с кучей важных вопросов. Простой и
+              практичный чат созданный для всех наших клиентов и в частности
+              именно Вас
+            </p>
+          </div>
+        </NavLink>
+        {/* <NavLink
           to='#'
           onClick={() => alert('В процессе разработки!')}
           className='relative group min-h-[180px] ss:min-h-[220px] py-2 flex justify-center items-center bg-pay'
@@ -108,7 +131,7 @@ const GBServices = () => {
               моментально и самое главное безопасно
             </p>
           </div>
-        </NavLink>
+        </NavLink> */}
         <NavLink
           to='/gb-buyer'
           className='relative group min-h-[180px] ss:min-h-[220px] py-2 flex justify-center items-center bg-buyer lg:bg-transparent'
@@ -155,7 +178,7 @@ const GBServices = () => {
             </p>
           </div>
         </NavLink>
-        <NavLink
+        {/* <NavLink
           to='/gb-chat'
           className='relative group min-h-[180px] ss:min-h-[220px] py-2 flex justify-center items-center'
         >
@@ -177,7 +200,7 @@ const GBServices = () => {
               именно Вас
             </p>
           </div>
-        </NavLink>
+        </NavLink> */}
       </div>
     </div>
   );
