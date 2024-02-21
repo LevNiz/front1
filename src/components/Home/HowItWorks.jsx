ac01b5bd397802f1f51903988f5bc98d233d2c9c
beks <turgunbaaev@gmail.com>
2024-02-21 14:31:02 +0300

    Refactor code

--- src/components/Home/HowItWorks.jsx
+++ src/components/Home/HowItWorks.jsx
@@ -6,11 +6,9 @@ import line2 from './../../assets/icons/how-works-line2.svg';
 
 const HowItWorks = () => {
   return (
-    <div
-      className='min-h-[400px] bg-colBgGray3 bg-center bg-cover bg-no-repeat py-16'
-    >
+    <div className='min-h-[400px] bg-colBgGray3 bg-center bg-cover bg-no-repeat py-16'>
       <div className='flex justify-center mb-5'>
-        <div className="flex relative bg-[#E8E8E8] w-max pr-2 rounded-sm">
+        <div className='flex relative bg-[#E8E8E8] w-max pr-2 rounded-sm'>
           <span className='absolute w-1 h-full bg-colYellow'></span>
           <h6 className='ml-3'>Процесс работы</h6>
         </div>
@@ -20,7 +18,7 @@ const HowItWorks = () => {
       </h1>
       <div className='md:flex-row flex-col items-center flex justify-between mt-24 content space-y-5 md:space-y-0'>
         <div className='max-w-[280px] md:min-h-[260px] mm:mr-[270px] md:mr-0 w-full text-center'>
-          <img className='mx-auto' src={icon1} alt="*" />
+          <img className='mx-auto' src={icon1} alt='*' />
           <div className='mt-5'>
             <h3 className='text-2xl md:text-3xl mb-3 lg:text-[40px] font-bold lg:pt-2'>
               Заказ
@@ -31,10 +29,14 @@ const HowItWorks = () => {
           </div>
         </div>
         <div>
-          <img className='my-16 md:my-0 mm:rotate-0 rotate-[40deg]' src={line1} alt="*" />
+          <img
+            className='my-16 md:my-0 mm:rotate-0 rotate-[40deg]'
+            src={line1}
+            alt='*'
+          />
         </div>
         <div className='max-w-[280px] md:min-h-[260px] mm:ml-[270px] md:ml-0 w-full text-center'>
-          <img className='mx-auto' src={icon2} alt="*" />
+          <img className='mx-auto' src={icon2} alt='*' />
           <div className='mt-5'>
             <h3 className='text-2xl md:text-3xl mb-3 lg:text-[40px] font-bold lg:pt-2'>
               Ожидание
@@ -45,10 +47,14 @@ const HowItWorks = () => {
           </div>
         </div>
         <div>
-          <img className='my-16 md:my-0 mm:rotate-0 rotate-[90deg]' src={line2} alt="*" />
+          <img
+            className='my-16 md:my-0 mm:rotate-0 rotate-[90deg]'
+            src={line2}
+            alt='*'
+          />
         </div>
         <div className='max-w-[280px] md:min-h-[260px] mm:mr-[270px] md:mr-0 w-full text-center'>
-          <img className='mx-auto' src={icon3} alt="*" />
+          <img className='mx-auto' src={icon3} alt='*' />
           <div className='mt-5'>
             <h3 className='text-2xl md:text-3xl mb-3 lg:text-[40px] font-bold lg:pt-2'>
               Доставка
